f69c063ef9dcf92ecbc130950e59abaa06412316
BrightMonthSpeech <88915459+BrightMonthSpeech@users.noreply.github.com>
2023-03-25 20:36:09 +0800

    Add files via upload

---
+++ day1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class day1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("他并没有什么做用");
+		int a=114514;
+		int b=114514;
+		int c=a*b;
+		int d=c*a;
+		System.out.println("来自先辈的回答"+d);
+		System.out.println(+a);
+		System.out.println(+b);
+		System.out.println(+c);
+        
+    }
+    
+}
